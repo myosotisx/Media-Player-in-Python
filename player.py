3029021a621e4173b42b1c69dbd6c65c99168b86
myosotisx <sthuiyi6@163.com>
2019-12-02 17:09:11 +0800

    实现暂停和停止功能

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import threading
 import time
+import os
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtMultimediaWidgets import QVideoWidget
@@ -49,7 +50,7 @@ class Player(QMainWindow, Ui_MainWindow):
         self.client_rtcp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_rtcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        #init thread
+        # init thread
         self.client_rtp_thread = None
         self.play_event = None
 
@@ -60,6 +61,9 @@ class Player(QMainWindow, Ui_MainWindow):
         self.client_session_id = None
         self.url = None
         self.status = self.IDLE
+
+        # init cache file
+        self.cache_filename = r'C:\Users\Myosotis\Videos\tmp.ts'
 
     def close_rtsp_connection(self):
         self.client_rtsp_socket.shutdown(socket.SHUT_RDWR)
@@ -86,12 +90,14 @@ class Player(QMainWindow, Ui_MainWindow):
         self.client_rtsp_socket.send(request.encode())
         response = self.client_rtsp_socket.recv(1024).decode()
         if rtsp.get_status_code(response) != 200:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
             return -1
         response_dict = rtsp.get_response_dict(response)
         if int(response_dict.get('CSeq')) != self.seq:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
             return -1
         self.seq += 1
@@ -101,17 +107,20 @@ class Player(QMainWindow, Ui_MainWindow):
         self.client_rtsp_socket.send(request.encode())
         response = self.client_rtsp_socket.recv(1024).decode()
         if rtsp.get_status_code(response) != 200:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
             return -1
         response_dict = rtsp.get_response_dict(response)
         if int(response_dict.get('CSeq')) != self.seq:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
             return -1
         self.client_rtp_port = util.match_rtp_port(response)
         if not self.client_rtp_port:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: can not specify RTP port.')
             return -1
         self.client_rtcp_port = self.client_rtp_port+1
@@ -127,28 +136,33 @@ class Player(QMainWindow, Ui_MainWindow):
         self.client_rtsp_socket.send(request.encode())
         response = self.client_rtsp_socket.recv(1024).decode()
         if rtsp.get_status_code(response) != 200:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
             return -1
         response_dict = rtsp.get_response_dict(response)
         if int(response_dict.get('CSeq')) != self.seq:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
             return -1
         self.client_session_id = int(response_dict.get('Session'))
         self.seq += 1
+        self.status = self.READY
         # send PLAY
         request_dict = {'CSeq': str(self.seq), 'Session': self.client_session_id, 'Range': 'npt=0.000-'}
         request = rtsp.generate_request('PLAY', url, request_dict)
         self.client_rtsp_socket.send(request.encode())
         response = self.client_rtsp_socket.recv(1024).decode()
         if rtsp.get_status_code(response) != 200:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
             return -1
         response_dict = rtsp.get_response_dict(response)
         if int(response_dict.get('CSeq')) != self.seq:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
             return -1
         self.seq += 1
@@ -158,10 +172,13 @@ class Player(QMainWindow, Ui_MainWindow):
         cur_seq = 0
         file = open(cache_filename, 'wb')
         while True:
-            data = self.client_rtp_socket.recv(rtp.TS_RTP_PACKET_SIZE)
+            try:
+                data = self.client_rtp_socket.recv(rtp.TS_RTP_PACKET_SIZE)
+            except:
+                break
             seq = rtp_packet.get_seq(data)
             if seq and seq < cur_seq:
-                print('packet loss')
+                print('Packet loss.')
                 continue
             cur_seq = seq
             payload = rtp_packet.get_payload(data)
@@ -169,31 +186,9 @@ class Player(QMainWindow, Ui_MainWindow):
                 self.play_event.wait()
             if self.status == self.IDLE:
                 break
-            print('play')
             file.write(payload)
-
-    def play_media(self):
-        if self.status == self.IDLE:
-            # setup and play
-            url = self.urlLineEdit.text()
-            res = self.setup_play(url)
-            if res != -1:
-                self.url = url
-                cache_file = r'C:\Users\Myosotis\Videos\tmp.ts'
-                self.client_rtp_thread = threading.Thread(target=self.recv_stream, args=(cache_file,))
-                self.status = self.PLAY
-                self.play_event = threading.Event()
-                self.client_rtp_thread.start()
-                self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(cache_file)))
-                self.timer.timeout.connect(self.start_play)
-                self.timer.start(5000)
-        elif self.status == self.READY:
-            # resume
-            self.status = self.PLAY
-            self.play_event.set()
-            self.media_player.play()
-        else:
-            return
+        print('Exit RTP thread.')
+        file.close()
 
     def start_play(self):
         self.timer.disconnect()
@@ -203,30 +198,128 @@ class Player(QMainWindow, Ui_MainWindow):
         self.media_player.stop()
         self.status = self.IDLE
 
+    def play_media(self):
+        if self.status == self.IDLE:
+            # setup and play
+            url = self.urlLineEdit.text()
+            res = self.setup_play(url)
+            if res != -1:
+                # remove cache file
+                os.remove(self.cache_filename)
+                self.url = url
+                self.client_rtp_thread = threading.Thread(target=self.recv_stream, args=(self.cache_filename,))
+                self.status = self.PLAY
+                self.play_event = threading.Event()
+                self.client_rtp_thread.start()
+                self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(self.cache_filename)))
+                self.timer.timeout.connect(self.start_play)
+                self.timer.start(5000)
+        elif self.status == self.READY:
+            # send PLAY
+            request_dict = {'CSeq': str(self.seq), 'Session': self.client_session_id, 'Range': 'npt=now-'}
+            request = rtsp.generate_request('PLAY', self.url, request_dict)
+            self.client_rtsp_socket.send(request.encode())
+            response = self.client_rtsp_socket.recv(1024).decode()
+            if rtsp.get_status_code(response) != 200:
+                # self.close_rtsp_connection()
+                self.destroy_connection()
+                QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
+                return
+            response_dict = rtsp.get_response_dict(response)
+            if int(response_dict.get('CSeq')) != self.seq:
+                # self.close_rtsp_connection()
+                self.destroy_connection()
+                QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
+                return
+            self.seq += 1
+            # resume
+            self.status = self.PLAY
+            self.play_event.set()
+            self.media_player.play()
+        else:
+            return
+
     def pause_media(self):
         if self.status != self.PLAY:
             return
+        self.media_player.pause()
+        self.play_event.clear()
+        self.status = self.READY
         # send PAUSE
         request_dict = {'CSeq': str(self.seq), 'Session': self.client_session_id}
         request = rtsp.generate_request('PAUSE', self.url, request_dict)
         self.client_rtsp_socket.send(request.encode())
         response = self.client_rtsp_socket.recv(1024).decode()
         if rtsp.get_status_code(response) != 200:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
             return -1
         response_dict = rtsp.get_response_dict(response)
         if int(response_dict.get('CSeq')) != self.seq:
-            self.close_rtsp_connection()
+            # self.close_rtsp_connection()
+            self.destroy_connection()
             QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
             return -1
         self.seq += 1
-        self.media_player.pause()
-        self.play_event.clear()
-        self.status = self.READY
 
     def stop_media(self):
-        pass
+        if self.status == self.IDLE:
+            return
+        request_dict = {'CSeq': str(self.seq), 'Session': self.client_session_id}
+        request = rtsp.generate_request('TEARDOWN', self.url, request_dict)
+        self.client_rtsp_socket.send(request.encode())
+        response = self.client_rtsp_socket.recv(1024).decode()
+        if rtsp.get_status_code(response) != 200:
+            # self.close_rtsp_connection()
+            self.destroy_connection()
+            QMessageBox.warning(self, 'Warning', 'Error: unexpected server response code.')
+            return
+        response_dict = rtsp.get_response_dict(response)
+        if int(response_dict.get('CSeq')) != self.seq:
+            # self.close_rtsp_connection()
+            self.destroy_connection()
+            QMessageBox.warning(self, 'Warning', 'Error: unexpected server response SN.')
+            return
+        self.seq += 1
+        self.destroy_connection()
+
+    def destroy_connection(self):
+        # reset status
+        self.status = self.IDLE
+
+        # reset player
+        self.media_player.stop()
+        self.media_player.setMedia(QMediaContent())
+
+        # reset RTSP socket
+        self.client_rtsp_socket.shutdown(socket.SHUT_RDWR)
+        self.client_rtsp_socket.close()
+        self.client_rtsp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_rtsp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # reset RTP socket
+        self.client_rtp_socket.shutdown(socket.SHUT_RDWR)
+        self.client_rtp_socket.close()
+        self.client_rtp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_rtp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # reset RTCP socket
+        self.client_rtcp_socket.shutdown(socket.SHUT_RDWR)
+        self.client_rtcp_socket.close()
+        self.client_rtcp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client_rtcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # reset thread
+        self.client_rtp_thread = None
+        self.play_event = None
+
+        # reset client parameters
+        self.seq = 0
+        self.client_rtp_port = None
+        self.client_rtcp_port = None
+        self.client_session_id = None
+        self.url = None
 
 
 if __name__ == "__main__":
